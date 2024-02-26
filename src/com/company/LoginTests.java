e1b7bbdf4bc894f85c38072c18e7b77beb6d18ab
Ivana <bosilkovska.ivana@gmail.com>
2024-02-26 20:39:51 +0100

    added new tests for Login form

---
+++ src/com/company/LoginTests.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class LoginTests {
+
+    public static void main(String[] args) {
+
+        System.out.println("test 1...");
+
+        System.out.println("test2...");
+
+    }
+}
